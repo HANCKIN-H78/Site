dd32ff1657f558850882c887aa872e414bb46553
lucas alexandre <lucasbez14@outlook.com>
2021-02-03 15:33:18 -0300

    secret

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="css" href="/css/index.css"/>
+    <title>Secret_Sociedade</title>
+</head>
+<body>
+
+    <header>
+
+        <Section class="cabecalho-logo">
+            <img src="/img/logo.png" alt="logo da secret-sociedade">
+            <h1>Secret Sociedade</h1>
+            <h3>We Are AnOnYmoUs</h3>
+
+            <form method="GET" id="form-login">
+                <label>E-mail</label>
+                <br>
+                <input type="text" id="email" placeholder="exemplo@email.com">
+                <br>
+                <label>Senha:</label>
+                <br>
+                <input type="password" id="senha" placeholder="********">
+                <br>
+                <input type="submit" value="Login">
+
+                <p>
+                    <a href="#">Seja parte dessa causa</a>
+                </p>
+            </form> 
+
+        </Section>
+
+        <nav>
+
+            <p>
+
+                <ul>
+                    <li>
+                        <a href="#">exemplo1</a>
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <a href="#">exemplo2</a>
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <a href="#">exemplo3</a>
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <a href="#">exemplo4</a>
+                    </li>
+                </ul>
+
+            </p>
+
+        </nav>
+    
+    </header>
+
+    <main>
+
+        <article>
+
+            <h1>Secret news</h1>
+            <p>lorem ipsum</p>
+            <img src="#" alt="news">
+
+        </article>
+
+
+    </main>
+
+    <footer>
+
+        <p></p>
+
+    </footer>
+
+
+</body>
+</html>
+
+
