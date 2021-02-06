50616bf9e18f1ad51e31d9a330848622539fc4d6
lucas alexandre <lucasbez14@outlook.com>
2021-02-06 11:33:18 -0300

    secret

---
+++ news.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--icone da pagina -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/icon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/icon/favicon-16x16.png">
+    <link rel="manifest" href="img/icon/site.webmanifest">
+
+    <!-- css -->
+    <link rel="stylesheet"  href="/css/news.css"/>
+    <title>Secret_Sociedade</title>
+</head>
+<body>
+
+    <header class="topo-pagina">
+
+        <Section class="cabecalho-logo">
+
+                <section class="login-area">
+
+                    <label class="email-text">E-mail:</label>
+                    <input type="text" class="email" placeholder="exemplo@email.com">
+                    <label class="senha-text">Senha:</label>
+                    <input type="password" class="senha" placeholder="********">
+                    <input type="submit" value="Login" class="btn-login">
+                    <br>
+                    <a href="#" class="link-conta">Criar uma conta</a> 
+
+                </section>
+
+            <img src="/img/logo.png" alt="logo da secret-sociedade" class="img-secret" width=" 340px">
+            <h2 class="subtitulo-logo">We Are AnOnYmoUs</h2>
+
+        </Section>
+    </header>
+
+        <nav class="menu">
+            <a href="/news.php" class="news">Noticias</a>
+            <a href="#" class="quem-somos">Quem somos</a>
+            <a href="#" class="aprenda">Aprenda</a>
+            <a href="#" class="nossa-causa">Nossa Causa</a>
+        </nav>
+    
+    
+
+    <main class="content">
+
+        <p class="terminal-aba1"> <b class="terminal-2">X</b> <b class="terminal-1">-</b></p>
+        <br>
+        <p class="terminal">
+        ┌──(<a href="https://www.kali.org/downloads/" target="_blanc" class="hide-link">root💀kali</a>)-[/home/root]
+        <br>        
+        └─# nmap -vv 8.8.8.8<b class="hide-link">|</b>        
+        </p>
+
+        <article class="news-area">
+
+            <h1>Secret💀news</h1>
+
+            <section class="news-section">
+                <section class="borda">
+                    <a href="#" class="link">
+                        <h2>Noticia um</h2>
+                        <img src="/img/img1.jpg" alt="news" class="img-news">
+                        <p class="news-text"> 
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        Impedit voluptates fugit consequatur quibusdam saepe tempore accusamus suscipit 
+                        obcaecati quos numquam, sed voluptatum asperiores optio, perspiciatis consectetur 
+                        delectus earum, quia exercitationem.
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        Impedit voluptates fugit consequatur quibusdam saepe tempore accusamus suscipit 
+                        obcaecati quos numquam, sed voluptatum asperiores optio, perspiciatis consectetur 
+                        delectus earum, quia exercitationem.
+                        </p>
+                    </a>
+                </section>
+
+                <section class="borda">
+                    <a href="#" class="link">
+                        <h2>Noticia dois</h2>
+                        <img src="/img/img2.jpg" alt="news" class="img-news">
+                        <p class="news-text">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        Impedit voluptates fugit consequatur quibusdam saepe tempore accusamus suscipit 
+                        obcaecati quos numquam, sed voluptatum asperiores optio, perspiciatis consectetur 
+                        delectus earum, quia exercitationem.
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        Impedit voluptates fugit consequatur quibusdam saepe tempore accusamus suscipit 
+                        obcaecati quos numquam, sed voluptatum asperiores optio, perspiciatis consectetur 
+                        delectus earum, quia exercitationem.
+                        </p>
+                    </a>
+                </section>
+
+                <section class="borda">
+                    <a href="#" class="link">
+                        <h2>Noticia tres</h2>
+                        <img src="/img/img1.jpg" alt="news" class="img-news">
+                        <p class="news-text">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        Impedit voluptates fugit consequatur quibusdam saepe tempore accusamus suscipit 
+                        obcaecati quos numquam, sed voluptatum asperiores optio, perspiciatis consectetur 
+                        delectus earum, quia exercitationem.
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        Impedit voluptates fugit consequatur quibusdam saepe tempore accusamus suscipit 
+                        obcaecati quos numquam, sed voluptatum asperiores optio, perspiciatis consectetur 
+                        delectus earum, quia exercitationem.
+                        </p>
+                    </a>
+                </section>
+            </section>
+
+        </article>
+
+
+    </main>
+
+    <footer>
+
+        <p class="texto-footer">Todos os direitos reservados a Secret_Sociedade &copy;
+        <?php
+        echo date("Y");
+        ?>
+        </p>
+
+    </footer>
+
+
+</body>
+</html>
+
+
